6ed3b3bae870ce246b2bde104eb31f335e8aaadf
Calvin Corde <mail@calvincorde.com>
2021-03-17 13:09:04 +0100

    Added placeholder on all sides

--- my-app/src/summary/JournalSummaryDetail.js
+++ my-app/src/summary/JournalSummaryDetail.js
@@ -2,6 +2,28 @@ export default function JournalSummaryDetail() {
     return (
         <div>
             <h3>This is Journal Summary Detail</h3>
+            <div>
+                <h3>15. March 2012</h3>
+                <p>Cum endant. Escim re nonsed quaspe nat quae doluptation</p>
+             </div>
+            <div>
+                <h3>16. March 2012</h3>
+                <p>Cum endant. Escim re nonsed quaspe nat quae doluptation</p>
+            </div>
+            <div>
+                <h3>17. March 2012</h3>
+                <p>Cum endant. Escim re nonsed quaspe nat quae doluptation</p>
+                <h3>15. March 2012</h3>
+                <p>Cum endant. Escim re nonsed quaspe nat quae doluptation</p>
+             </div>
+            <div>
+                <h3>16. March 2012</h3>
+                <p>Cum endant. Escim re nonsed quaspe nat quae doluptation</p>
+            </div>
+            <div>
+                <h3>17. March 2012</h3>
+                <p>Cum endant. Escim re nonsed quaspe nat quae doluptation</p>
+            </div>
         </div>
     )
 }
