90e1cefb452fe4272efcccf4b97731b6b92d1785
Calvin Corde <mail@calvincorde.com>
2021-03-19 13:33:17 +0100

    finalised the rooting, deleted some unnecessary routes and checked everything

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,11 +10,8 @@ import MainPage from "./MainPage"
 import MainEntry from "./entry/MainEntry"
 
 import MainSummary from "./summary/MainSummary"
-import MoodSummary from "./summary/MoodSummary"
 import MoodSummaryDetail from "./summary/MoodSummaryDetail"
-import TagsSummary from "./summary/TagsSummary"
 import TagsSummaryDetail from "./summary/TagsSummaryDetail"
-import JournalSummary from "./summary/JournalSummary"
 import JournalSummaryDetail from "./summary/JournalSummaryDetail"
 import Navigation from './Navigation';
 
@@ -28,11 +25,8 @@ function App() {
         <Switch>
           <Route path="/entry" component={MainEntry} />
           <Route path="/summary/MainSummary" component={MainSummary} />
-          <Route path="/summary/MoodSummary" component={MoodSummary} />
           <Route path="/summary/MoodSummaryDetail" component={MoodSummaryDetail} />
-          <Route path="/summary/TagsSummary" component={TagsSummary} />
           <Route path="/summary/TagsSummaryDetail" component={TagsSummaryDetail} />
-          <Route path="/summary/JournalSummary" component={JournalSummary} />
           <Route path="/summary/JournalSummaryDetail" component={JournalSummaryDetail} />
           <Route exact path="/" component={MainPage} />
         </Switch>
--- my-app/src/summary/MainSummary.js
+++ my-app/src/summary/MainSummary.js
@@ -1,8 +1,15 @@
 import { Link } from "react-router-dom";
 
+import { 
+  Route, 
+  Switch
+} from "react-router-dom";
+
+
 import MoodSummary from "./MoodSummary"
 import TagsSummary from "./TagsSummary"
 import JournalSummary from "./JournalSummary"
+
 
 
 export default function MainSummary() {
